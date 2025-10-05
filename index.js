ce1bc67f68d6df12596877a367716c5a3e0a5aa8
aliudn23 <aliudn@gmail.com>
2025-10-05 12:15:53 +0700

    fixing deploy mongo connect

--- index.js
+++ index.js
@@ -290,6 +290,6 @@ app.delete('/tasks/:id', async (req, res) => {
     }
 });
 
-app.listen(8080, "https://simply-api.onrender.com", () => {
+app.listen(3000, "https://simply-api.onrender.com", () => {
     console.log('Server is running on https://simply-api.onrender.com');
 });
